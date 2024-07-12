a3f32c370e024a0c4afadb558750e0a58f64419b
DevikaRNair05 <devikarajeesh05@gmail.com>
2024-07-12 19:45:58 +0530

    connect login page with sqllite

--- loginapp/lib/main.dart
+++ loginapp/lib/main.dart
@@ -4,16 +4,18 @@ import 'database_helper.dart';
 // import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
       routes: {
-        '/about': (context) => AboutUsPage(),
+        '/about': (context) => const AboutUsPage(),
       },
     );
   }
@@ -25,46 +27,48 @@ class LoginPage extends StatelessWidget {
 
   final DatabaseHelper databaseHelper = DatabaseHelper();
 
+  LoginPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Center(
           child: SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Text(
+                const Text(
                   'Welcome back',
                   style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
                   textAlign: TextAlign.center,
                 ),
-                SizedBox(height: 16.0),
-                Text(
+                const SizedBox(height: 16.0),
+                const Text(
                   'Login to your account',
                   style: TextStyle(fontSize: 18.0),
                   textAlign: TextAlign.center,
                 ),
-                SizedBox(height: 32.0),
+                const SizedBox(height: 32.0),
                 TextField(
                   controller: emailController,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     labelText: 'Email',
                     border: OutlineInputBorder(),
                   ),
                 ),
-                SizedBox(height: 16.0),
+                const SizedBox(height: 16.0),
                 TextField(
                   controller: passwordController,
                   obscureText: true,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     labelText: 'Password',
                     border: OutlineInputBorder(),
                   ),
                 ),
-                SizedBox(height: 8.0),
+                const SizedBox(height: 8.0),
                 ElevatedButton(
                   onPressed: () async {
                     String email = emailController.text.trim();
@@ -80,11 +84,11 @@ class LoginPage extends StatelessWidget {
                         context: context,
                         builder: (BuildContext context) {
                           return AlertDialog(
-                            title: Text('Invalid Credentials'),
-                            content: Text('Please check your email and password.'),
+                            title: const Text('Invalid Credentials'),
+                            content: const Text('Please check your email and password.'),
                             actions: <Widget>[
                               TextButton(
-                                child: Text('OK'),
+                                child: const Text('OK'),
                                 onPressed: () {
                                   Navigator.of(context).pop();
                                 },
@@ -95,50 +99,50 @@ class LoginPage extends StatelessWidget {
                       );
                     }
                   },
-                  child: Text('Login now'),
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.green,
                   ),
+                  child: const Text('Login now'),
                 ),
-                SizedBox(height: 16.0),
+                const SizedBox(height: 16.0),
                 ElevatedButton.icon(
                   onPressed: () {
                     // Handle login with Google
                   },
-                  icon: Icon(Icons.g_translate),
-                  label: Text('Continue with Google'),
+                  icon: const Icon(Icons.g_translate),
+                  label: const Text('Continue with Google'),
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.black,
                   ),
                 ),
-                // SizedBox(height: 16.0),
-                // ElevatedButton(
-                //   onPressed: () async {
-                //     // Insert a user for testing purposes
-                //     await databaseHelper.insertUser('test1@example.com', 'password123');
-                //     showDialog(
-                //       context: context,
-                //       builder: (BuildContext context) {
-                //         return AlertDialog(
-                //           title: Text('User Inserted'),
-                //           content: Text('A test user has been inserted.'),
-                //           actions: <Widget>[
-                //             TextButton(
-                //               child: Text('OK'),
-                //               onPressed: () {
-                //                 Navigator.of(context).pop();
-                //               },
-                //             ),
-                //           ],
-                //         );
-                //       },
-                //     );
-                //   },
-                //   child: Text('Insert Test User'),
-                //   style: ElevatedButton.styleFrom(
-                //     backgroundColor: Colors.blue,
-                //   ),
-                //),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () async {
+                    // Insert a user for testing purposes
+                    await databaseHelper.insertUser('test1@example.com', 'password123');
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('User Inserted'),
+                          content: const Text('A test user has been inserted.'),
+                          actions: <Widget>[
+                            TextButton(
+                              child: const Text('OK'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: const Text('Insert Test User'),
+                ),
               ],
             ),
           ),
