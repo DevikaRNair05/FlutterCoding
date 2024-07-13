a066f55cdb42a38959cb1f0941afef9cb0ac1d87
DevikaRNair05 <devikarajeesh05@gmail.com>
2024-07-13 19:30:33 +0530

    Call Button App

---
+++ call_button_app/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Call Button App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String phoneNumber = '+919544519440';
+  const MyHomePage({super.key});
+     
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Call Button App'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _makePhoneCall,
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white, backgroundColor: Colors.blue, // Text color
+          ),
+          child: const Text('Make a Call'),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _makePhoneCall() async {
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+
+    if (await canLaunchUrl(launchUri)) {
+      await launchUrl(launchUri);
+    } else {
+      throw 'Could not launch $launchUri';
+    }
+  }
+}
